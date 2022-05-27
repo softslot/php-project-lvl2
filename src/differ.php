9810614e148a4a60a6bd28524b46a2021c38ae9a
Konstantin Korolenko <softslot-com@yandex.ru>
2022-05-27 18:09:43 +0300

    update src/differ.php

--- src/differ.php
+++ src/differ.php
@@ -4,5 +4,46 @@ namespace Hexlet\Code\Differ;
 
 function genDiff($firstFile, $secondFile)
 {
-    return;
+    $file1 = json_decode(file_get_contents($firstFile), true);
+    $file2 = json_decode(file_get_contents($secondFile), true);
+
+    $keys1 = array_keys($file1);
+    $keys2 = array_keys($file2);
+    $uniqueKeys = array_unique([...$keys1, ...$keys2]);
+    sort($uniqueKeys);
+
+    $symbols = ['add' => '+', 'delete' => '-', 'no change' => ' '];
+    $data = [];
+    foreach ($uniqueKeys as $key) {
+        $value1 = $file1[$key] ?? null;
+        $value2 = $file2[$key] ?? null;
+        $normalizedValue1 = normalizeValue($value1);
+        $normalizedValue2 = normalizeValue($value2);
+
+        if (isset($value1) && isset($value2)) {
+            if ($value1 === $value2) {
+                $data[] = "{$symbols['no change']} {$key} : {$normalizedValue1}";
+            } else {
+                $data[] = "{$symbols['delete']} {$key} : {$normalizedValue1}";
+                $data[] = "{$symbols['add']} {$key} : {$normalizedValue2}";
+            }
+        } elseif (isset($value1)) {
+            $data[] = "{$symbols['delete']} {$key} : {$normalizedValue1}";
+        } else {
+            $data[] = "{$symbols['add']} {$key} : {$normalizedValue2}";
+        }
+    }
+
+    $str = implode("\n  ", $data);
+    print("{\n  {$str}\n}\n");
+}
+
+function normalizeValue($value): string
+{
+    return match ($value) {
+        false => 'false',
+        true => 'true',
+        null => 'null',
+        default => $value,
+    };
 }
