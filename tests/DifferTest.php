68c7d155cd784f53783b4918c39fe0d5ae4bc34a
Konstantin Korolenko <softslot-com@yandex.ru>
2022-08-09 16:07:54 +0300

    update tests

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -10,22 +10,36 @@ class DifferTest extends TestCase
     private string $filePathJson2 = __DIR__ . '/fixtures/file2.json';
     private string $filePathYaml1 = __DIR__ . '/fixtures/file1.yml';
     private string $filePathYaml2 = __DIR__ . '/fixtures/file2.yml';
-    private string $expected;
+    private string $expectedOutputStylish;
+    private string $expectedOutputPlain;
 
     protected function setUp(): void
     {
-        $this->expected = file_get_contents(__DIR__ . '/fixtures/expected.txt');
+        $this->expectedOutputStylish = file_get_contents(__DIR__ . '/fixtures/expected_output_stylish.txt');
+        $this->expectedOutputPlain = file_get_contents(__DIR__ . '/fixtures/expected_output_plain.txt');
     }
 
-    public function testDiffJson(): void
+    public function testDiffJsonStylish(): void
     {
         $result = genDiff($this->filePathJson1, $this->filePathJson2);
-        $this->assertEquals($this->expected, $result);
+        $this->assertEquals($this->expectedOutputStylish, $result);
     }
 
-    public function testDiffYaml(): void
+    public function testDiffYamlStylish(): void
     {
         $result = genDiff($this->filePathYaml1, $this->filePathYaml2);
-        $this->assertEquals($this->expected, $result);
+        $this->assertEquals($this->expectedOutputStylish, $result);
+    }
+
+    public function testDiffJsonPlain(): void
+    {
+        $result = genDiff($this->filePathJson1, $this->filePathJson2, 'plain');
+        $this->assertEquals($this->expectedOutputPlain, $result);
+    }
+
+    public function testDiffYamlPlain(): void
+    {
+        $result = genDiff($this->filePathYaml1, $this->filePathYaml2, 'plain');
+        $this->assertEquals($this->expectedOutputPlain, $result);
     }
 }
