2eb70114869f26683032eadfe73eed2388a9f41c
Konstantin Korolenko <softslot-com@yandex.ru>
2022-08-09 14:39:27 +0300

    updated tests

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -8,8 +8,8 @@ class DifferTest extends TestCase
 {
     private string $filePathJson1 = __DIR__ . '/fixtures/file1.json';
     private string $filePathJson2 = __DIR__ . '/fixtures/file2.json';
-//    private string $filePathYaml1 = __DIR__ . '/fixtures/file1.yml';
-//    private string $filePathYaml2 = __DIR__ . '/fixtures/file2.yml';
+    private string $filePathYaml1 = __DIR__ . '/fixtures/file1.yml';
+    private string $filePathYaml2 = __DIR__ . '/fixtures/file2.yml';
     private string $expected;
 
     protected function setUp(): void
@@ -17,15 +17,15 @@ class DifferTest extends TestCase
         $this->expected = file_get_contents(__DIR__ . '/fixtures/expected.txt');
     }
 
-    public function testDiffJson()
+    public function testDiffJson(): void
     {
         $result = genDiff($this->filePathJson1, $this->filePathJson2);
         $this->assertEquals($this->expected, $result);
     }
 
-//    public function testDiffYaml()
-//    {
-//        $result = genDiff($this->filePathYaml1, $this->filePathYaml2);
-//        $this->assertEquals($this->expected, $result);
-//    }
+    public function testDiffYaml(): void
+    {
+        $result = genDiff($this->filePathYaml1, $this->filePathYaml2);
+        $this->assertEquals($this->expected, $result);
+    }
 }
