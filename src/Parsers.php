29f4c7a1dcf63a83010a0528f01f9c5d68cbffa3
Konstantin Korolenko <softslot-com@yandex.ru>
2022-08-17 15:45:17 +0300

    refacroring

--- src/Parsers.php
+++ src/Parsers.php
@@ -5,7 +5,7 @@ namespace Differ\Parsers;
 use function Differ\Parsers\JsonParser\parseJson;
 use function Differ\Parsers\YamlParser\parseYaml;
 
-function parse(string $extension, string $data): object
+function parse(string $extension, string|false $data): object
 {
     return match ($extension) {
         'json'        => parseJson($data),
