552015558a16382cd2860bda5fce71e38c055f29
Konstantin Korolenko <softslot-com@yandex.ru>
2022-08-18 16:57:25 +0300

    refactoring generatePlainOutput

--- src/Formatters/PlainFormatter.php
+++ src/Formatters/PlainFormatter.php
@@ -25,23 +25,17 @@ function generatePlainOutput(array $tree, array $propertyNames): array
 {
     $output = array_map(function ($child) use ($propertyNames): string|array {
         $name = implode('.', [...$propertyNames, $child['name']]);
-        switch ($child['type']) {
-            case 'added':
-                $value = stringify($child['newValue']);
-                return "Property '{$name}' was added with value: {$value}";
-            case 'removed':
-                return "Property '{$name}' was removed";
-            case 'changed':
-                $oldValue = stringify($child['oldValue']);
-                $newValue = stringify($child['newValue']);
-                return "Property '{$name}' was updated. From {$oldValue} to {$newValue}";
-            case 'unchanged':
-                return "";
-            case 'nested':
-                return generatePlainOutput($child['children'], [...$propertyNames, $child['name']]);
-            default:
-                throw new \Exception("Invalid node type: '{$child['type']}'");
-        }
+        $stringifyOldValue = stringify($child['oldValue']);
+        $stringifyNewValue = stringify($child['newValue']);
+
+        return match ($child['type']) {
+            'added'     => "Property '{$name}' was added with value: {$stringifyNewValue}",
+            'removed'   => "Property '{$name}' was removed",
+            'changed'   => "Property '{$name}' was updated. From {$stringifyOldValue} to {$stringifyNewValue}",
+            'unchanged' => '',
+            'nested'    => generatePlainOutput($child['children'], [...$propertyNames, $child['name']]),
+            default     => throw new \Exception("Invalid node type: '{$child['type']}'"),
+        };
     }, $tree);
 
     $filteredOutput = array_filter($output);
