4ade7517a5e2efc87e7b896e8acae62f1666bce6
Konstantin Korolenko <softslot-com@yandex.ru>
2022-08-18 17:21:34 +0300

    refactoring

--- src/Parsers/JsonParser.php
+++ src/Parsers/JsonParser.php
@@ -2,7 +2,10 @@
 
 namespace Differ\Parsers\JsonParser;
 
+/**
+ * @throws \JsonException
+ */
 function parseJson(string $data): object
 {
-    return json_decode($data, false);
+    return json_decode($data, false, 512, JSON_THROW_ON_ERROR);
 }
