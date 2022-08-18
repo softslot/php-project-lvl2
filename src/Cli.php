84edb37017b7ec0328230c0d9b14423a331c99c3
Konstantin Korolenko <softslot-com@yandex.ru>
2022-08-18 16:44:20 +0300

    add Cli.php

---
+++ src/Cli.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Differ\Cli;
+
+use Docopt;
+
+use function Differ\Differ\genDiff;
+
+/**
+ * @throws \Exception
+ */
+function run(): string
+{
+    $doc = <<<DOC
+
+    Generate diff
+    
+    Usage:
+      gendiff (-h|--help)
+      gendiff (-v|--version)
+      gendiff [--format <fmt>] <firstFile> <secondFile>
+    
+    Options:
+      -h --help                     Show this screen
+      -v --version                  Show version
+      --format <fmt>                Report format [default: stylish]
+    
+    DOC;
+
+    $args = Docopt::handle($doc, array('version' => '1.0.0'));
+    $diff = genDiff($args['<firstFile>'], $args['<secondFile>'], $args['--format']);
+
+    return "{$diff}\n";
+}
