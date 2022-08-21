67185ffd281dc88f057b9905ab36363b375ecbce
Konstantin Korolenko <softslot-com@yandex.ru>
2022-08-21 18:23:13 +0300

    delete Formatters/JsonFormatter.php

--- src/Formatters.php
+++ src/Formatters.php
@@ -2,7 +2,6 @@
 
 namespace Differ\Formatters;
 
-use function Differ\Formatters\JsonFormatter\render as renderJson;
 use function Differ\Formatters\PlainFormatter\render as renderPlain;
 use function Differ\Formatters\StylishFormatter\render as renderStylish;
 
@@ -12,7 +11,7 @@ use function Differ\Formatters\StylishFormatter\render as renderStylish;
 function format(array $data, string $format): string
 {
     return match ($format) {
-        'json'    => renderJson($data),
+        'json'    => json_encode($data, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT),
         'plain'   => renderPlain($data),
         'stylish' => renderStylish($data),
         default   => throw new \Exception("Undefended format: '{$format}'"),
